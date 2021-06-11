888a48c8a3f2d706e7bb1ad4e6cb8e4356460615
Jon West <electrikjesus@gmail.com>
2021-06-11 12:36:00 -0400

    Fix overlays for Android 11

--- agp-apps.mk
+++ agp-apps.mk
@@ -1,12 +1,9 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(my-dir)
 
 PRODUCT_COPY_FILES := \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.freeform_window_management.xml
 
-# Copy any Permissions files, overriding anything if needed
-$(foreach f,$(wildcard $(LOCAL_PATH)/app/permissions/*.xml),\
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/$(notdir $f)))
-    
+# Copy any Permissions files, overriding anything if needed    
 $(foreach f,$(wildcard $(LOCAL_PATH)/priv-app/permissions/*.xml),\
     $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/$(notdir $f)))
 
@@ -15,4 +12,4 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/foss_privapp/permissions/*.xml),\
     $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/$(notdir $f)))
 endif
 
-DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+#~ DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
