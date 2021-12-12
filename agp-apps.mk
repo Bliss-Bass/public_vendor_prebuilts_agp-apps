e05a0950b2b4e6a6cff112f838be9765640c761e
Jon West <electrikjesus@gmail.com>
2021-12-12 18:39:46 -0500

    Wrap wallpaper inclusion under a conditional
    USE_AGP_WALLPAPER=true

--- agp-apps.mk
+++ agp-apps.mk
@@ -3,4 +3,7 @@ LOCAL_PATH := $(call my-dir)
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.freeform_window_management.xml
 
-#~ DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+ifeq ($(USE_AGP_WALLPAPER),true)
+DEVICE_PACKAGE_OVERLAYS += vendor/prebuilts/agp-apps/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/prebuilts/agp-apps/overlay
+endif
