2bf2ae36f3d9ca3ac5edc02fae99026b65b30bca
electrikjesus <electrikjesus@gmail.com>
2024-03-06 21:03:20 -0500

    apps: update garlic-player app

--- garlic_player/Android.mk
+++ garlic_player/Android.mk
@@ -6,7 +6,7 @@ ifeq ("$(USE_BLISS_GARLIC_LAUNCHER)","true")
 include $(CLEAR_VARS)
 LOCAL_MODULE := com.sagiadinos.garlic.player
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := bin/com.sagiadinos.garlic.player_683.apk
+LOCAL_SRC_FILES := bin/com.sagiadinos.garlic.player.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_OVERRIDES_PACKAGES := 
